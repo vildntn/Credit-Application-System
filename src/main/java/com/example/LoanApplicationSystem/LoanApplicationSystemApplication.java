193ec7c6e4f20363241b1995782e2fdd7e4d242a
vildntn <vildant35@gmail.com>
2022-02-15 00:16:41 +0300

    project initialized

---
+++ src/main/java/com/example/LoanApplicationSystem/LoanApplicationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoanApplicationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplicationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanApplicationSystemApplication.class, args);
+	}
+
+}
