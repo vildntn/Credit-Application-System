ca093f01a9349d7af63b90da1df51a3bc2664935
vildntn <vildant35@gmail.com>
2022-02-18 22:46:56 +0300

    LoanResult Controller added

---
+++ src/main/java/com/example/LoanApplicationSystem/controller/LoanResultController.java
@@ -0,0 +1,36 @@
+package com.example.LoanApplicationSystem.controller;
+
+
+import com.example.LoanApplicationSystem.model.entity.LoanResult;
+import com.example.LoanApplicationSystem.service.LoanResultService;
+import com.example.LoanApplicationSystem.service.constant.Messages;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/loanResult")
+public class LoanResultController {
+
+    @Autowired
+    private LoanResultService loanResultService;
+
+
+    @GetMapping("/getAllLoanResult")
+    public List<LoanResult> getAllLoanApplication(){
+        return loanResultService.getAllLoanResult();
+    }
+
+    @GetMapping("/getLoanResultById")
+    public LoanResult getLoanResultById(int id){
+        return loanResultService.getLoanResultById(id);
+    }
+
+    @PostMapping("/addLoanResult")
+    public void addLoanResult( @RequestBody LoanResult loanResult){
+        loanResultService.addLoanResult(loanResult);
+    }
+
+
+}
