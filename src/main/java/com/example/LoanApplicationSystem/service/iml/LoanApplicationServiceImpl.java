25d885c8cb0c5607b811c5b53230cab8211ab112
vildntn <vildant35@gmail.com>
2022-02-18 22:44:25 +0300

    LoanApplicationServiceImpl added

---
+++ src/main/java/com/example/LoanApplicationSystem/service/iml/LoanApplicationServiceImpl.java
@@ -0,0 +1,58 @@
+package com.example.LoanApplicationSystem.service.iml;
+
+import com.example.LoanApplicationSystem.exception.NotFoundException;
+import com.example.LoanApplicationSystem.model.entity.Customer;
+import com.example.LoanApplicationSystem.model.entity.LoanApplication;
+import com.example.LoanApplicationSystem.repository.LoanApplicationRepository;
+import com.example.LoanApplicationSystem.service.CustomerService;
+import com.example.LoanApplicationSystem.service.LoanApplicationService;
+import com.example.LoanApplicationSystem.service.LoanScoreService;
+import com.example.LoanApplicationSystem.service.LoanService;
+import com.example.LoanApplicationSystem.service.constant.Messages;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LoanApplicationServiceImpl implements LoanApplicationService {
+
+    @Autowired
+    private LoanApplicationRepository loanApplicationRepository;
+
+    @Autowired
+    private CustomerService customerService;
+
+
+    @Override
+    public String addLoanApplication(LoanApplication loanApplication) {
+        loanApplicationRepository.save(loanApplication);
+        return Messages.loanApplicationAdded;
+    }
+
+    @Override
+    public String deleteLoanApplication(int id) {
+        loanApplicationRepository.delete(getLoanApplicationById(id));
+        return Messages.loanApplicationDeleted;
+    }
+
+    @Override
+    public LoanApplication updateLoanApplication(LoanApplication loanApplication) {
+        return null;
+    }
+
+    @Override
+    public LoanApplication getLoanApplicationById(int id) {
+        Optional<LoanApplication> loanApplicationById = loanApplicationRepository.findById(id);
+        return loanApplicationById.orElseThrow(()->new RuntimeException("Loan Application doesn't found"));
+    }
+
+    @Override
+    public List<LoanApplication> getAllLoanApplication() {
+        return loanApplicationRepository.findAll();
+    }
+
+
+
+}
