c5cd864b6d7265b0b297d2d13a2f7a22af5f62ab
vildntn <vildant35@gmail.com>
2022-02-21 15:55:20 +0300

    LoanResult Service updated

--- src/main/java/com/example/LoanApplicationSystem/service/LoanResultService.java
+++ src/main/java/com/example/LoanApplicationSystem/service/LoanResultService.java
@@ -1,11 +1,12 @@
 package com.example.LoanApplicationSystem.service;
 
+import com.example.LoanApplicationSystem.model.entity.LoanApplication;
 import com.example.LoanApplicationSystem.model.entity.LoanResult;
 
 import java.util.List;
 
 public interface LoanResultService {
-    String addLoanResult(LoanResult loanResult);
+    void addLoanResult(LoanResult loanResult);
 
     boolean deleteLoanResult(int id);
 
@@ -14,4 +15,6 @@ public interface LoanResultService {
     LoanResult getLoanResultById(int id);
 
     List<LoanResult> getAllLoanResult();
+
+    String checkLoanResultByLoanApplication(int id);
 }
