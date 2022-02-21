7f60b06853e12eeff586d426c81a1cb8464fe30f
vildntn <vildant35@gmail.com>
2022-02-21 15:55:53 +0300

    Updated service (#9)
    * LoanApplication Service updated
    
    * LoanResult Service updated
    
    * LoanScore Service updated

--- src/main/java/com/example/LoanApplicationSystem/service/LoanScoreService.java
+++ src/main/java/com/example/LoanApplicationSystem/service/LoanScoreService.java
@@ -2,13 +2,17 @@ package com.example.LoanApplicationSystem.service;
 
 import com.example.LoanApplicationSystem.model.entity.LoanScore;
 
+import java.util.List;
+
 public interface LoanScoreService {
 
     void addLoanScore(LoanScore loanScore);
     boolean deleteLoanScore(int id);
     LoanScore updateLoanScore(LoanScore loanScore);
     LoanScore getLoanScoreById(int id);
-
+    List<LoanScore> getAllLoanScore();
     int getLoanScoreByCustomerId(int id);
+
+    LoanScore getLoanScoreByCustomerIdentNumber(String identificationNumber);
 
 }
