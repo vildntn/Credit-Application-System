fb151f86640dec7f466fb4f9f6cbb531716d4f06
vildntn <vildant35@gmail.com>
2022-02-25 01:08:51 +0300

    LoanResult Repository deleted

--- src/main/java/com/example/LoanApplicationSystem/repository/LoanResultRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.LoanApplicationSystem.repository;
-
-import com.example.LoanApplicationSystem.model.entity.LoanResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoanResultRepository extends JpaRepository<LoanResult,Integer> {
-}
