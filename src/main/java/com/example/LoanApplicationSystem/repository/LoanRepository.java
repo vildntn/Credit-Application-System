c5c5367191c52c0c67680de0c787a9d9aa097e52
vildntn <vildant35@gmail.com>
2022-02-25 01:08:08 +0300

    Loan repository deleted

--- src/main/java/com/example/LoanApplicationSystem/repository/LoanRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.LoanApplicationSystem.repository;
-
-import com.example.LoanApplicationSystem.model.entity.Loan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoanRepository extends JpaRepository<Loan, Integer> {
-    Loan getLoanByCustomerId(int id);
-}
