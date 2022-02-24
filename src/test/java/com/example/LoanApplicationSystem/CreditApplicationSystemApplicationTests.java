f5097d9b58fe1ce6a0176cd88b4b603ef2f629c8
vildntn <vildant35@gmail.com>
2022-02-25 02:01:49 +0300

    Feature tests (#19)
    * CreditScore test added
    
    * CustomerService test added
    
    * CreditApplication service test added
    
    * test
    
    * test deleted

--- src/test/java/com/example/LoanApplicationSystem/CreditApplicationSystemApplicationTests.java
+++ src/test/java/com/example/LoanApplicationSystem/CreditApplicationSystemApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LoanApplicationSystemApplicationTests {
+class CreditApplicationSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
---
+++ src/test/java/com/example/LoanApplicationSystem/service/iml/CreditScoreServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.example.LoanApplicationSystem.service.iml;
+
+import com.example.LoanApplicationSystem.repository.CreditScoreRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CreditScoreServiceImplTest {
+
+    @Mock
+    private CreditScoreRepository creditScoreRepository;
+
+    @InjectMocks
+    private CreditScoreServiceImpl loanScoreService;
+
+    @Test
+    void addCreditScore() {
+    }
+
+    @Test
+    void deleteCreditScore() {
+    }
+
+    @Test
+    void getCreditScoreByCustomerNationalID() {
+    }
+
+    @Test
+    void getCreditScoreById() {
+    }
+
+    @Test
+    void getAllCreditScore() {
+    }
+
+    @Test
+    void getCreditScoreByCustomerId() {
+    }
+
+    @Test
+    void createCreditScoreToCustomer() {
+    }
+}
