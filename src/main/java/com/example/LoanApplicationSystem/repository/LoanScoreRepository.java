4d0be8131cedd7c950b7c6ada79672b21800c72b
vildntn <vildant35@gmail.com>
2022-02-25 01:08:35 +0300

    LoanScore Repository changed & deleted

--- src/main/java/com/example/LoanApplicationSystem/repository/LoanScoreRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.LoanApplicationSystem.repository;
-
-import com.example.LoanApplicationSystem.model.entity.LoanScore;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoanScoreRepository extends JpaRepository<LoanScore,Integer> {
-    int getLoanScoreByCustomerId(int id);
-}
