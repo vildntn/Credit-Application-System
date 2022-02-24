1a16073ae990dfa35f44ead1f27069f74c72d6eb
vildntn <vildant35@gmail.com>
2022-02-25 02:01:08 +0300

    test

---
+++ src/test/java/com/example/LoanApplicationSystem/CreditApplicationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoanApplicationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
