0d0220288b3ed679228deceee007d737a841ab08
vildntn <vildant35@gmail.com>
2022-02-25 22:49:29 +0300

    Messages updated

--- src/main/java/com/example/CreditApplicationSystem/service/constant/Messages.java
+++ src/main/java/com/example/CreditApplicationSystem/service/constant/Messages.java
@@ -7,10 +7,14 @@ public class Messages {
     public static String creditApplicationAdded="Your credit application has been received.";
     public static String creditApplicationDeleted="Your credit application has been deleted.";
     public static String creditApplicationDoesntFound="No credit application found!!";
+    public static String creditApplicationAlreadyExist="You have already made a credit application.";
 
 
     /*Messages for customer class*/
     public static String customerNotFound="Customer not found!";
+
+    /*Messages for credit score class*/
+    public static String creditScoreNotFound="Credit Score doesn't found";
 
 
 
