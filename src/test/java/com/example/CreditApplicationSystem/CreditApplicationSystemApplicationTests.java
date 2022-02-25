8d898283e6234cd0f7a1acf0168614a98d87df77
vildntn <vildant35@gmail.com>
2022-02-25 12:02:25 +0300

    name changed

--- src/test/java/com/example/CreditApplicationSystem/CreditApplicationSystemApplicationTests.java
+++ src/test/java/com/example/CreditApplicationSystem/CreditApplicationSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.LoanApplicationSystem;
+package com.example.CreditApplicationSystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
