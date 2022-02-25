d5efc421a683737bc6cdc43dd193aa537550d3a5
vildntn <vildant35@gmail.com>
2022-02-26 01:55:13 +0300

    AlreadyExistException added

---
+++ src/main/java/com/example/CreditApplicationSystem/exception/AlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.example.CreditApplicationSystem.exception;
+
+public class AlreadyExistException extends RuntimeException{
+    public AlreadyExistException(String message){
+        super(message);
+    }
+}
