e8524d52fba94d9775b655f010307c7fb29e9a8d
vildntn <vildant35@gmail.com>
2022-02-27 18:52:56 +0300

    user table added

---
+++ src/main/java/com/example/CreditApplicationSystem/model/entity/User.java
@@ -0,0 +1,34 @@
+package com.example.CreditApplicationSystem.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Size(min = 5, message = "Minimum password length: 5 characters")
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = {
+            @JoinColumn(name = "user_id")}, inverseJoinColumns = {
+            @JoinColumn(name = "role_id")})
+    public List<Role> roles;
+
+}
