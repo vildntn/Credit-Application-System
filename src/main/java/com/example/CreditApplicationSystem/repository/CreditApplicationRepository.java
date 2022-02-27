fe60afc4333f6a0ac82e821dc4a448120702e428
vildntn <vildant35@gmail.com>
2022-02-27 11:36:13 +0300

    added new method

--- src/main/java/com/example/CreditApplicationSystem/repository/CreditApplicationRepository.java
+++ src/main/java/com/example/CreditApplicationSystem/repository/CreditApplicationRepository.java
@@ -4,4 +4,5 @@ import com.example.CreditApplicationSystem.model.entity.CreditApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CreditApplicationRepository extends JpaRepository<CreditApplication, Integer> {
+    CreditApplication findByCustomer_NationalID(String nationalID);
 }
